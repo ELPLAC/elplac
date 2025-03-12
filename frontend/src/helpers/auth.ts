957ace7192435319f71993d142995953d34643ce
Florencia-digitalok <floppyharmath@hotmail.com>
2025-03-12 12:44:56 -0300

    login

--- frontend/src/helpers/auth.ts
+++ frontend/src/helpers/auth.ts
@@ -2,15 +2,17 @@ import { decodeJWT } from "./decoder";
 
 export const isTokenExpired = (token: string): boolean => {
   try {
-    const decodedToken = decodeJWT(token);
-    if (!decodedToken || !decodedToken.exp) {
+    const decodedToken = decodeJWT(token); 
 
-      return true;
+    if (!decodedToken || !decodedToken.exp) {
+      console.warn('El token no tiene campo de expiración o no es válido');
+      return true; 
     }
+
     const expirationTime = decodedToken.exp * 1000; 
     return Date.now() > expirationTime; 
   } catch (error) {
-
+    console.error('Error al verificar si el token ha expirado:', error);
     return true; 
   }
 };
--- frontend/src/helpers/decoder.ts
+++ frontend/src/helpers/decoder.ts
@@ -1,16 +1,24 @@
 export const decodeJWT = (token: string) => {
   try {
-    const base64Url = token.split(".")[1];
-    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const parts = token.split(".");
+    if (parts.length !== 3) {
+      console.error("Token JWT inválido. Debe contener 3 partes.");
+      return null;
+    }
+
+    const base64Url = parts[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/"); // Reemplazamos para base64 estándar
+
     const jsonPayload = decodeURIComponent(
       atob(base64)
         .split("")
-        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)) // Escapamos los caracteres especiales
         .join("")
     );
 
-    return JSON.parse(jsonPayload);
+    return JSON.parse(jsonPayload); 
   } catch (error) {
-    return;
+    console.error("Error al decodificar el token JWT:", error);
+    return null; 
   }
 };
