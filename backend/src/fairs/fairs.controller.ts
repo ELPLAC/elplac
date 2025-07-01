5232b225689ab30d6b62f6977131d4edbdf37a68
Nicolas Aballay <thedarkin32@gmail.com>
2025-07-01 13:30:20 -0300

    rebuild

--- backend/src/fairs/fairs.controller.ts
+++ backend/src/fairs/fairs.controller.ts
@@ -1,16 +1,13 @@
 import { Controller, Delete, Param, UseGuards } from '@nestjs/common';
 import { FairsService } from './fairs.service';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { Roles } from '../auth/roles.decorator';
-import { RolesGuard } from '../auth/roles.guard';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('fairs')
 export class FairsController {
   constructor(private readonly fairsService: FairsService) {}
 
   @Delete(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
+  @UseGuards(AuthGuard)
   async deleteFair(@Param('id') id: string) {
     return this.fairsService.deleteFair(id);
   }
