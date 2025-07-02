d079e3f08380945b053f428296b485e1152afcbf
Nicolas Aballay <thedarkin32@gmail.com>
2025-07-02 13:40:16 -0300

    rebuild

--- frontend/src/helpers/getUserFromLocalStorage.ts
+++
@@ -1,7 +0,0 @@
-export function getUserFromLocalStorage() {
-  if (typeof window !== 'undefined') {
-    const user = localStorage.getItem('user');
-    return user ? JSON.parse(user) : null;
-  }
-  return null;
-}
