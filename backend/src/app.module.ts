e04cbb407420bc4df45b00c5e77854fc7074d871
Nicolas Aballay <thedarkin32@gmail.com>
2026-07-17 11:38:58 -0300

    ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -44,7 +44,7 @@ import { AppController } from 'app.controller';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      isGlobal: tru,
       load: [typeorm],
     }),
     TypeOrmModule.forRootAsync({
